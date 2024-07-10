98355a35dc2c40e7027cdab91c5489dbb0e85646
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-10 10:47:21 -0500

    Add and setup linters

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import pluginJs from "@eslint/js";
+import configPrettier from 'eslint-config-prettier';
+import pluginImportSort from 'eslint-plugin-simple-import-sort';
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import parserVue from 'vue-eslint-parser'
+
+
+export default tseslint.config(
+  { ignores: ['dist/', 'node_modules/'] },
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { 
+    languageOptions: {
+      globals: globals.browser,
+      parser: parserVue,
+      parserOptions: {
+        parser: tseslint.parser,
+      }
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    plugins: {
+      'simple-import-sort': pluginImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    }
+  },
+  configPrettier,
+);
