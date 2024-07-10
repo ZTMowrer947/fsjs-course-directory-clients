89f3319a3449ba693c3504d9948a437ee740c0ae
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-10 12:06:27 -0500

    Setup Vue router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import './style.css';
 
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import HomePage from './pages/HomePage.vue';
 
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: '/', component: HomePage }],
+});
+
+createApp(App).use(router).mount('#app');
