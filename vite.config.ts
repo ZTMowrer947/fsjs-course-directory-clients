1ca4dad6c42cf25a88f992f2fd41509e63affcb7
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-10 11:43:22 -0500

    Configure Vite for TS path alises

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import tsPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [tsPaths(), vue()],
 });
