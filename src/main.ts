7c528da0503aa3eb0a294549061714a0eb244d32
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-11 09:00:13 -0500

    Setup basic ListPage

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import './style.css';
 
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 import App from './App.vue';
-import HomePage from './pages/HomePage.vue';
+import ListPage from './routes/courses/ListPage.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: {
+      name: 'course-list',
+    },
+  },
+  {
+    path: '/courses',
+    name: 'course-list',
+    component: ListPage,
+  },
+] satisfies readonly RouteRecordRaw[];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/', component: HomePage }],
+  routes,
 });
 
 createApp(App).use(router).mount('#app');
---
+++ src/routes/courses/ListPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This will be a detail page</h1>
+</template>
