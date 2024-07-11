7248d3d7e7c84aaebdda8154e3a3267d97da527b
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-11 10:43:14 -0500

    Update ListItem to conform to test

--- src/routes/courses/ListItem.vue
+++ src/routes/courses/ListItem.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>{{ course.title }}</h1>
+  <RouterLink :to>
+    <h1>{{ course.title }}</h1>
+  </RouterLink>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { RouterLink, RouterLinkProps } from 'vue-router';
+
 import type { CoursePreview } from '~/entities/course.ts';
 
-defineProps<{ course: CoursePreview }>();
+const props = defineProps<{ course: CoursePreview }>();
+
+// Setup router link 'to' prop
+const to = computed<RouterLinkProps['to']>(() => `/courses/${encodeURIComponent(props.course.id)}`);
 </script>
