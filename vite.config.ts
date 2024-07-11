0a52f8ab1b3b43c6b67ac9c3fc51e18ca58d5bbe
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-11 17:53:37 -0500

    Configure Vite to process Vue JSX
    To be used for Cypress component tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vite';
 import tsPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsPaths(), vue()],
+  plugins: [tsPaths(), vue(), vueJsx()],
 });
