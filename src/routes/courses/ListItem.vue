89db476e17e2604c2ef0ba18a1c5f602f70de54d
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-11 10:13:06 -0500

    Write ListItem to comply with test as-is

---
+++ src/routes/courses/ListItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>{{ course.title }}</h1>
+</template>
+
+<script setup lang="ts">
+import type { CoursePreview } from '~/entities/course.ts';
+
+defineProps<{ course: CoursePreview }>();
+</script>
