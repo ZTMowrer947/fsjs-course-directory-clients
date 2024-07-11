cf629a24c6ef47966c485b51396b1c8197c45503
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-11 09:18:06 -0500

    Add basic course list query

--- src/routes/courses/ListPage.vue
+++ src/routes/courses/ListPage.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>This will be a detail page</h1>
+  <div>
+    <h1>This will be a detail page</h1>
+    <ul v-if="data">
+      <li v-for="course in data" :key="course.id">
+        {{ course.title }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
+
+const { data } = useQuery({
+  queryKey: ['courses'],
+  queryFn: async () => {
+    const res = await fetch('http://localhost:5000/api/courses');
+
+    return res.json();
+  },
+});
+</script>
