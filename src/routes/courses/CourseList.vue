da2921c92eb019c60ca04faa2b271ab655e771d7
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-12 05:53:51 -0500

    Add styling for CourseList

--- src/routes/courses/CourseList.vue
+++ src/routes/courses/CourseList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <ListItem v-for="course in courses" :key="course.id" :course="course" />
+  <div class="grid grid-cols gap-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <ListItem v-for="course in courses" :key="course.id" :course="course" data-testid="course-item" />
   </div>
 </template>
 
