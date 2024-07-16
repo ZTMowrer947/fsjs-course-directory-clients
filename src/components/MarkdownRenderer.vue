19f2701da0cfbbd0137dd193294e52916ae43279
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-16 11:50:26 -0500

    Add component for rendering Markdown

---
+++ src/components/MarkdownRenderer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div v-html="markdown.render(source)"></div>
+</template>
+
+<script setup lang="ts">
+import MarkdownIt from 'markdown-it';
+
+const markdown = new MarkdownIt();
+
+defineProps<{ source: string }>();
+</script>
