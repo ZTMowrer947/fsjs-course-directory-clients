226af5b49893ed880bfd789c7c4f7a3bdf0b07c2
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-16 12:38:18 -0500

    Add author attribution to CourseDetails

--- src/routes/courses/[id]/CourseDetails.vue
+++ src/routes/courses/[id]/CourseDetails.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <h1>{{ course.title }}</h1>
+    <h3>{{ authorText }}</h3>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
 import type { CourseDetail } from '~/entities/course.ts';
 
-defineProps<{
+const props = defineProps<{
   course: CourseDetail;
 }>();
+
+const authorText = computed(() => {
+  const authorName = [props.course.user.firstName, props.course.user.lastName].join(' ');
+
+  return `By ${authorName}`;
+});
 </script>
