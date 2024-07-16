180bedefe00c3b744360fb45f6fb40e54034de1f
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-16 12:44:40 -0500

    Add course description with Markdown to CourseDetails

--- src/routes/courses/[id]/CourseDetails.vue
+++ src/routes/courses/[id]/CourseDetails.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <h1>{{ course.title }}</h1>
-    <h3>{{ authorText }}</h3>
+    <div>
+      <h1>{{ course.title }}</h1>
+      <h3>{{ authorText }}</h3>
+    </div>
+    <MarkdownRenderer :source="course.description" />
   </div>
 </template>
 
@@ -9,6 +12,9 @@
 import { computed } from 'vue';
 
 import type { CourseDetail } from '~/entities/course.ts';
+
+// DEBUG: Why does using ~ shorthand to access component not work?
+import MarkdownRenderer from '../../../components/MarkdownRenderer.vue';
 
 const props = defineProps<{
   course: CourseDetail;
