98089890e967cf416e714543c6bb6e23f1a05eb4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-16 11:25:51 -0500

    Add single course query

--- src/routes/courses/queries.ts
+++ src/routes/courses/queries.ts
@@ -1,7 +1,19 @@
-import type { CoursePreview } from '~/entities/course.ts';
+import type { CourseDetail, CoursePreview } from '~/entities/course.ts';
 
 export async function fetchCourseList(): Promise<CoursePreview[]> {
   const res = await fetch('http://localhost:5000/api/courses');
 
   return res.json();
+}
+
+export async function fetchSingleCourse(id: CourseDetail['id']): Promise<CourseDetail | null> {
+  const url = `http://localhost:5000/api/courses/${encodeURIComponent(id)}`;
+
+  const res = await fetch(url);
+
+  if (res.status === 404) {
+    return null;
+  } else {
+    return res.json();
+  }
 }
