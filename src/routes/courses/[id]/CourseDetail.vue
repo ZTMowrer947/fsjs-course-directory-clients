889651a9cc06780129cee4f6c006ac9d031c5ede
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-16 10:46:27 -0500

    Add basic CourseDetail component

---
+++ src/routes/courses/[id]/CourseDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ course.title }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { CourseDetail } from '~/entities/course.ts';
+
+defineProps<{
+  course: CourseDetail;
+}>();
+</script>
