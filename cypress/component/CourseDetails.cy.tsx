9c69b14f10a6ca2ab32a1fb9776f9ab6d6624c41
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-17 08:39:39 -0500

    Add second CourseDetails test for optional course props

--- cypress/component/CourseDetails.cy.tsx
+++ cypress/component/CourseDetails.cy.tsx
@@ -4,7 +4,7 @@ import type { CourseDetail } from '~/entities/course.ts';
 import CourseDetails from '~/routes/courses/[id]/CourseDetails.vue';
 
 describe('CourseDetails component', () => {
-  it('renders course title and description', () => {
+  it('renders course title, author, and description', () => {
     // Generate fake course data
     const desc = faker.lorem.paragraph();
     const listEntry = faker.lorem.words(4);
@@ -34,5 +34,46 @@ describe('CourseDetails component', () => {
     // Find paragraph and list item in description
     cy.findByRole('paragraph').should('have.text', desc);
     cy.findByRole('listitem').should('have.text', listEntry);
+
+    // Expect no stats container to exist
+    cy.findByTestId('stats').should('not.exist');
+  });
+
+  it('additionally renders time estimate and needed materials when provided', () => {
+    // Generate fake course with time estimated and material list
+    const estimatedTime = `${faker.number.int({ min: 1, max: 16 })} hours`;
+    const materialsList = Array.from({ length: 5 }, () => faker.lorem.words(2));
+    const materialsNeeded = materialsList.map((item) => `- ${item}`).join('\n');
+
+    const course = {
+      id: faker.number.int({ min: 1, max: 10 }),
+      userId: faker.number.int({ min: 1, max: 10 }),
+      title: faker.lorem.words(3),
+      description: faker.lorem.paragraph(),
+      estimatedTime,
+      materialsNeeded,
+      user: {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+      },
+    } satisfies CourseDetail;
+
+    // Render component
+    cy.mount(<CourseDetails course={course} />);
+
+    // Find stats container, and query within it
+    cy.findByTestId('stats').within(() => {
+      // Find time estimate heading
+      cy.findByRole('heading', { level: 3 }).should('have.text', course.estimatedTime);
+
+      // Find material list items
+      cy.findAllByRole('listitem').should(($matList) => {
+        expect($matList, 'list of material items').to.have.length(materialsList.length);
+
+        materialsList.forEach((item, index) => {
+          expect($matList.get(index), `Material list item #${index + 1}`).to.have.text(item);
+        });
+      });
+    });
   });
 });
