b0f2a93de07ea488630f4ada3ba6758181d36256
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-22 12:12:32 -0500

    Add form and text plugins to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import formsPlugin from '@tailwindcss/forms';
+import typographyPlugin from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.vue', './cypress/**/*.{tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [formsPlugin, typographyPlugin],
 };
