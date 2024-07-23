7de4cf991640abbf8998b6d6b84db8a7133a9454
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-23 09:41:02 -0500

    Add PrimaryHeader and PrimaryLayout

---
+++ src/components/PrimaryHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h1 class="text-2xl font-bold">Courses</h1>
+
+    <nav v-if="!authPending">
+      <div v-if="user">
+        <span>Welcome {{ user.firstName }} {{ user.lastName }}!</span>
+        <button @click="handleSignout">Sign Out</button>
+      </div>
+      <div v-else>
+        <RouterLink to="/signin">Sign In</RouterLink>
+        <RouterLink to="/signup">Sign Up</RouterLink>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import { RouterLink } from 'vue-router';
+
+import type { User } from '~/entities/user.ts';
+
+defineProps({
+  user: {
+    type: Object as PropType<User>,
+    required: false,
+  },
+  authPending: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const emit = defineEmits<{
+  signout: [];
+}>();
+
+function handleSignout() {
+  emit('signout');
+}
+</script>
---
+++ src/components/PrimaryLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <PrimaryHeader :user="data ?? undefined" :auth-pending="isPending" @signout="signOut" />
+    <div class="mx-10" data-testid="main-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useQuery, useQueryClient } from '@tanstack/vue-query';
+import { inject } from 'vue';
+
+import { credentialManagerKey } from '~/injectKeys.ts';
+import { dummyCredentialManager } from '~/lib/credential.ts';
+import { getUserFromCredentials } from '~/routes/(auth)/queries.ts';
+import { userKeys } from '~/routes/(auth)/queryKeys.ts';
+
+import PrimaryHeader from './PrimaryHeader.vue';
+
+const credentialManager = inject(credentialManagerKey, dummyCredentialManager);
+const { data, isPending } = useQuery({
+  queryKey: userKeys.user,
+  queryFn() {
+    // Retrieve credentials, and if found validate them
+    const credentials = credentialManager.get();
+
+    if (!credentials) return null;
+
+    return getUserFromCredentials(credentials);
+  },
+});
+const queryClient = useQueryClient();
+
+function signOut() {
+  // Remove credentials and invalidate user query
+  credentialManager.clear();
+  queryClient.invalidateQueries({
+    queryKey: userKeys.user,
+  });
+}
+</script>
