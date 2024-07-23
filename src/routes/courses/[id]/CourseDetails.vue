adc8d2ab97ded613fbd02e6f87c0ce1da325e32b
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-23 17:40:00 -0500

    Tweak CourseDetails styling

--- src/routes/courses/[id]/CourseDetails.vue
+++ src/routes/courses/[id]/CourseDetails.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="grid grid-cols-6">
-    <div class="col-span-6">
-      <div class="mb-20">
-        <h1 class="text-4xl font-bold">{{ course.title }}</h1>
-        <h3 class="text-lg">{{ authorText }}</h3>
+    <div class="col-span-6 md:col-span-3 xl:col-span-4 mb-10">
+      <div class="mb-10">
+        <h1 class="text-4xl font-bold mb-2">{{ course.title }}</h1>
+        <h3 class="text-lg font-light">{{ authorText }}</h3>
       </div>
       <MarkdownRenderer class="prose" :source="course.description" />
     </div>
-    <div data-testid="stats" v-if="hasStats" class="col-span-6">
-      <div v-if="course.estimatedTime">
-        <h4 class="text-lg">Estimated time</h4>
-        <h3 class="text-xl">{{ course.estimatedTime }}</h3>
+    <div data-testid="stats" v-if="hasStats" class="col-span-6 md:col-start-5 md:xl:col-span-2 xl:col-start-6">
+      <div class="mb-2" v-if="course.estimatedTime">
+        <h4 class="text-lg border-b-2 mb-2">Estimated time</h4>
+        <h3 class="text-xl font-bold">{{ course.estimatedTime }}</h3>
       </div>
 
       <div v-if="course.materialsNeeded">
-        <h4 class="text-lg">Materials Needed</h4>
+        <h4 class="text-lg border-b-2 mb-2">Materials Needed</h4>
         <MarkdownRenderer class="prose" :source="course.materialsNeeded" />
       </div>
     </div>
