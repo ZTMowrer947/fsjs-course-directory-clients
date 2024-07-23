04a2478c1b4b5e8ee1a9d676a791df56f2edda9f
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-23 12:33:43 -0500

    Add course nav to DetailPage

--- src/routes/courses/[id]/DetailPage.vue
+++ src/routes/courses/[id]/DetailPage.vue
@@ -1,13 +1,33 @@
 <template>
   <PrimaryLayout>
-    <CourseDetail v-if="data" :course="data" />
+    <header class="border-b-2 py-3 px-10 flex justify-start" data-testid="course-nav">
+      <RouterLink
+        class="bg-indigo-500 hover:bg-indigo-700 transition-colors text-white font-bold rounded-lg p-3 me-2"
+        :to="updateTo"
+      >
+        Update Course
+      </RouterLink>
+      <RouterLink
+        class="bg-indigo-500 hover:bg-indigo-700 transition-colors text-white font-bold rounded-lg p-3 me-2"
+        :to="deleteTo"
+      >
+        Delete Course
+      </RouterLink>
+      <RouterLink
+        class="border-2 border-indigo-500 text-indigo-500 hover:bg-indigo-700 hover:border-indigo-700 hover:text-white transition-colors font-bold rounded-lg p-2.5"
+        to="/courses"
+      >
+        Back to List
+      </RouterLink>
+    </header>
+    <CourseDetail v-if="course" :course="course" />
   </PrimaryLayout>
 </template>
 
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query';
 import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
 
 import PrimaryLayout from '~/components/PrimaryLayout.vue';
 
@@ -18,8 +38,17 @@ import CourseDetail from './CourseDetails.vue';
 const route = useRoute();
 const id = computed(() => Number.parseInt(route.params.id.toString()));
 
-const { data } = useQuery({
+const updateTo = computed(() => `/courses/${encodeURIComponent(id.value)}/update`);
+const deleteTo = computed(() => `/courses/${encodeURIComponent(id.value)}/delete`);
+
+const { data: course } = useQuery({
   queryKey: courseKeys.byId(id.value),
   queryFn: () => fetchSingleCourse(id.value),
 });
 </script>
+
+<style scoped>
+header {
+  margin: -1.25rem -2.5rem 1.25rem;
+}
+</style>
