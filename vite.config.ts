4feec084ff4f9be15bd25e63f712fff734d59a20
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-23 07:54:20 -0500

    Adjust Vite alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'node:path';
+
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vite';
-import tsPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsPaths(), vue(), vueJsx()],
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '~': resolve('src'),
+    },
+  },
 });
