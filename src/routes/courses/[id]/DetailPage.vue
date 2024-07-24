0474fd7be2d015475164949333ce8e20737b181e
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-23 19:26:23 -0500

    Add auth logic for DetailPage
    Update/delete links only visible to course owner

--- src/routes/courses/[id]/DetailPage.vue
+++ src/routes/courses/[id]/DetailPage.vue
@@ -4,12 +4,14 @@
       <RouterLink
         class="bg-indigo-500 hover:bg-indigo-700 transition-colors text-white font-bold rounded-lg p-3 me-2"
         :to="updateTo"
+        v-if="course && authedUser?.id === course.userId"
       >
         Update Course
       </RouterLink>
       <RouterLink
         class="bg-indigo-500 hover:bg-indigo-700 transition-colors text-white font-bold rounded-lg p-3 me-2"
         :to="deleteTo"
+        v-if="course && authedUser?.id === course.userId"
       >
         Delete Course
       </RouterLink>
@@ -26,10 +28,14 @@
 
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query';
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 import { RouterLink, useRoute } from 'vue-router';
 
 import PrimaryLayout from '~/components/PrimaryLayout.vue';
+import { credentialManagerKey } from '~/injectKeys.ts';
+import { dummyCredentialManager } from '~/lib/credential.ts';
+import { getUserFromStoredCredentials } from '~/routes/(auth)/queries.ts';
+import { userKeys } from '~/routes/(auth)/queryKeys.ts';
 
 import { fetchSingleCourse } from '../queries.ts';
 import courseKeys from '../queryKeys.ts';
@@ -41,9 +47,14 @@ const id = computed(() => Number.parseInt(route.params.id.toString()));
 const updateTo = computed(() => `/courses/${encodeURIComponent(id.value)}/update`);
 const deleteTo = computed(() => `/courses/${encodeURIComponent(id.value)}/delete`);
 
+const credentialManager = inject(credentialManagerKey, dummyCredentialManager);
 const { data: course } = useQuery({
   queryKey: courseKeys.byId(id.value),
   queryFn: () => fetchSingleCourse(id.value),
+});
+const { data: authedUser } = useQuery({
+  queryKey: userKeys.user,
+  queryFn: () => getUserFromStoredCredentials(credentialManager),
 });
 </script>
 
