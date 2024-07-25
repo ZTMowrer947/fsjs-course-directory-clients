2fc222e543d9691e0c57394e33de4cc8d102234c
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-25 13:05:36 -0500

    Add basic course create/update form

---
+++ src/components/CourseUpsertForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <form method="post">
+    <div>
+      <slot name="heading"></slot>
+
+      <div>
+        <label for="title">Course Title</label>
+        <input type="text" id="title" v-model.trim="title" />
+      </div>
+
+      <div>
+        <label for="description">Course Description</label>
+        <textarea id="description" v-model.trim="description"></textarea>
+      </div>
+    </div>
+    <div>
+      <div>
+        <label for="estimatedTime">Estimated Time</label>
+        <input type="text" id="estimatedTime" v-model.trim="estimatedTime" />
+      </div>
+      <div>
+        <label for="materialsNeeded">Materials Needed</label>
+        <textarea id="materialsNeeded" v-model.trim="materialsNeeded"></textarea>
+      </div>
+    </div>
+    <div>
+      <slot name="buttons"></slot>
+    </div>
+  </form>
+</template>
+
+<script lang="ts" setup>
+const title = defineModel<string>('title', {
+  required: true,
+});
+const description = defineModel<string>('description', {
+  required: true,
+});
+const estimatedTime = defineModel<string | null>('estimatedTime', {
+  required: true,
+  set(value) {
+    return value === '' ? null : value;
+  },
+  get(value) {
+    return value === null ? '' : value;
+  },
+});
+
+const materialsNeeded = defineModel<string | null>('materialsNeeded', {
+  required: true,
+  set(value) {
+    return value === '' ? null : value;
+  },
+  get(value) {
+    return value === null ? '' : value;
+  },
+});
+</script>
