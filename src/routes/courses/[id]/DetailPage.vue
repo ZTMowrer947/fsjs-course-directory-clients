76a66528fbbc56b314c33df7b9060ae739eadb34
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-29 20:41:51 -0500

    Refactor authed user query into composable

--- src/routes/courses/[id]/DetailPage.vue
+++ src/routes/courses/[id]/DetailPage.vue
@@ -4,14 +4,14 @@
       <RouterLink
         class="bg-indigo-500 hover:bg-indigo-700 transition-colors text-white font-bold rounded-lg p-3 me-2"
         :to="updateTo"
-        v-if="courseQuery.data.value && authedUser?.id === courseQuery.data.value.userId"
+        v-if="courseQuery.data.value && authedUserQuery.data.value?.id === courseQuery.data.value.userId"
       >
         Update Course
       </RouterLink>
       <RouterLink
         class="bg-indigo-500 hover:bg-indigo-700 transition-colors text-white font-bold rounded-lg p-3 me-2"
         :to="deleteTo"
-        v-if="courseQuery.data.value && authedUser?.id === courseQuery.data.value.userId"
+        v-if="courseQuery.data.value && authedUserQuery.data.value?.id === courseQuery.data.value.userId"
       >
         Delete Course
       </RouterLink>
@@ -27,17 +27,13 @@
 </template>
 
 <script setup lang="ts">
-import { useQuery } from '@tanstack/vue-query';
-import { computed, inject } from 'vue';
+import { computed } from 'vue';
 import { RouterLink } from 'vue-router';
 
 import PrimaryLayout from '~/components/PrimaryLayout.vue';
+import useAuthedUser from '~/composables/useAuthedUser.ts';
 import useCourseId from '~/composables/useCourseId.ts';
 import useCourseQuery from '~/composables/useCourseQuery.ts';
-import { credentialManagerKey } from '~/injectKeys.ts';
-import { dummyCredentialManager } from '~/lib/credential.ts';
-import { getUserFromStoredCredentials } from '~/routes/(auth)/queries.ts';
-import { userKeys } from '~/routes/(auth)/queryKeys.ts';
 
 import CourseDetail from './CourseDetails.vue';
 
@@ -46,12 +42,8 @@ const id = useCourseId();
 const updateTo = computed(() => `/courses/${encodeURIComponent(id.value)}/update`);
 const deleteTo = computed(() => `/courses/${encodeURIComponent(id.value)}/delete`);
 
-const credentialManager = inject(credentialManagerKey, dummyCredentialManager);
 const courseQuery = useCourseQuery(id);
-const { data: authedUser } = useQuery({
-  queryKey: userKeys.user,
-  queryFn: () => getUserFromStoredCredentials(credentialManager),
-});
+const authedUserQuery = useAuthedUser();
 </script>
 
 <style scoped>
