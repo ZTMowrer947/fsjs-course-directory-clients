7239cdaa7395b9c311ce32de56bb29dcb31cc69a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-31 09:36:30 -0500

    Refactor single-course routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
+import { useQueryClient } from '@tanstack/vue-query';
 import { inject } from 'vue';
 import { createRouter } from 'vue-router';
 
-import { credentialManagerKey } from './injectKeys';
-import { dummyCredentialManager } from './lib/credential';
+import { credentialManagerKey } from './injectKeys.ts';
+import { dummyCredentialManager } from './lib/credential.ts';
 import SignInPage from './routes/(auth)/signin/SignInPage.vue';
 import SignUpPage from './routes/(auth)/signup/SignUpPage.vue';
 import DeletePage from './routes/courses/[id]/delete/DeletePage.vue';
@@ -10,6 +11,8 @@ import DetailPage from './routes/courses/[id]/DetailPage.vue';
 import UpdatePage from './routes/courses/[id]/update/UpdatePage.vue';
 import CreatePage from './routes/courses/create/CreatePage.vue';
 import ListPage from './routes/courses/ListPage.vue';
+import { fetchSingleCourse } from './routes/courses/queries.ts';
+import courseKeys from './routes/courses/queryKeys.ts';
 
 type Routes = Parameters<typeof createRouter>[0]['routes'];
 
@@ -38,27 +41,41 @@ const routes: Routes = [
   },
   {
     path: '/courses/:id',
-    name: 'course-detail',
-    component: DetailPage,
-    meta: {
-      requiresAuth: false,
+    async beforeEnter(to) {
+      // Fetch course data ahead of navigation
+      const id = Number.parseInt(to.params.id.toString(), 10);
+      const queryClient = useQueryClient();
+
+      const course = await queryClient.ensureQueryData({
+        queryKey: courseKeys.byId(id),
+        queryFn: ({ queryKey }) => fetchSingleCourse(queryKey[1]),
+      });
+
+      // Abort navigation if course is not found
+      if (!course) {
+        return false;
+      }
     },
-  },
-  {
-    path: '/courses/:id/update',
-    name: 'update-course',
-    component: UpdatePage,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: '/courses/:id/delete',
-    name: 'delete-course',
-    component: DeletePage,
-    meta: {
-      requiresAuth: true,
-    },
+    children: [
+      {
+        path: '',
+        name: 'course-detail',
+        component: DetailPage,
+        meta: { requiresAuth: false },
+      },
+      {
+        path: 'update',
+        name: 'update-course',
+        component: UpdatePage,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: 'delete',
+        name: 'delete-course',
+        component: DeletePage,
+        meta: { requiresAuth: true },
+      },
+    ],
   },
   {
     path: '/signin',
