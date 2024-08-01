3ee55ac9c7f72c25fc035febbc5090284c74e290
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-08-01 09:28:04 -0500

    Implement course deletion on delete page

--- src/routes/courses/[id]/delete/DeletePage.vue
+++ src/routes/courses/[id]/delete/DeletePage.vue
@@ -1,6 +1,6 @@
 <template>
   <PrimaryLayout>
-    <div class="flex-1 grid grid-cols-6">
+    <div class="flex-1 grid grid-cols-6" v-if="courseQuery.data">
       <form class="col-start-3 col-end-5" method="post" @submit.prevent.stop="handleSubmit">
         <h1 class="text-2xl font-bold">WARNING!</h1>
 
@@ -9,6 +9,8 @@
           deletion.
         </p>
 
+        <p>{{ courseQuery.data.value?.title }}</p>
+
         <div class="flex flex-col">
           <label for="title">Title</label>
           <input class="rounded-lg focus:border-indigo-700" type="text" id="title" v-model="title" />
@@ -16,8 +18,9 @@
 
         <div class="grid grid-cols-6 gap-1 mt-2">
           <button
-            class="col-span-3 border-2 border-red-500 bg-red-500 hover:border-red-700 hover:bg-red-700 transition-colors text-white rounded-lg py-2"
+            class="col-span-3 border-2 border-red-500 bg-red-500 hover:border-red-700 hover:bg-red-700 disabled:border-gray-600 disabled:bg-gray-600 transition-colors text-white rounded-lg py-2"
             type="submit"
+            :disabled="courseQuery.data.value?.title !== title"
           >
             DELETE Course
           </button>
@@ -34,14 +37,38 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query';
+import { inject, ref } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
 
 import PrimaryLayout from '~/components/PrimaryLayout.vue';
+import useCourseId from '~/composables/useCourseId.ts';
+import { credentialManagerKey } from '~/injectKeys.ts';
+import { dummyCredentialManager } from '~/lib/credential.ts';
+
+import { deleteCourse } from '../../queries/delete.ts';
+import singleCourseQueryOpts from '../../queries/single.ts';
+import courseKeys from '../../queryKeys.ts';
 
 const title = ref('');
 
+const id = useCourseId();
+const courseQuery = useQuery(singleCourseQueryOpts(id.value));
+const credentialManager = inject(credentialManagerKey, dummyCredentialManager);
+const queryClient = useQueryClient();
+const router = useRouter();
+const deleteMutation = useMutation({
+  mutationFn: () => deleteCourse(id.value, credentialManager),
+  onSuccess: () => {
+    // Clear query data for delete course, redirect to listing
+    queryClient.invalidateQueries({
+      queryKey: courseKeys.byId(id.value),
+    });
+    router.push({ name: 'course-list' });
+  },
+});
+
 function handleSubmit() {
-  console.log(title.value);
+  deleteMutation.mutate();
 }
 </script>
