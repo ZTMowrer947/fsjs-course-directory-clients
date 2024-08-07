5f14b2a5d48afdd2358440cd611089203a386ff8
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-08-07 08:41:25 -0500

    Add ResultAsync wrapper for fetch

--- src/lib/result.ts
+++ src/lib/result.ts
@@ -1,4 +1,6 @@
-import type { Result, ResultAsync } from 'neverthrow';
+import { errAsync, okAsync, type Result,ResultAsync } from 'neverthrow';
+
+import { FetchFailureError, ResponseNotOkError, UnexpectedAppError } from '~/entities/errors';
 
 /**
  * Either returns the successful value or throws the failure value,
@@ -25,4 +27,33 @@ export function unwrapOrThrow<T, E>(result: Result<T, E>): T {
  */
 export async function asyncUnwrapOrReject<T, E>(result: ResultAsync<T, E>): Promise<T> {
   return result.then(unwrapOrThrow);
+}
+
+type FetchAsResultAsyncError = UnexpectedAppError | FetchFailureError | ResponseNotOkError;
+
+/**
+ * A ResultAsync wrapper for the Fetch API. Additionally exposes a failed response
+ * through the error type.
+ * @param options The options to pass to fetch.
+ * @returns A ResultAsync with the fetch Response as the Ok type and
+ * a fetch-related error as the Error type.
+ */
+export function fetchAsResultAsync(
+  ...options: Parameters<typeof fetch>
+): ResultAsync<Response, FetchAsResultAsyncError> {
+  return ResultAsync.fromPromise(fetch(...options), (err) => {
+    // If fetch threw an error, wrap it
+    if (err instanceof Error) {
+      throw new FetchFailureError(err);
+    } else {
+      throw new UnexpectedAppError();
+    }
+  }).andThen((res) => {
+    // If response failed, throw an error
+    if (!res.ok) {
+      return errAsync(new ResponseNotOkError(res));
+    } else {
+      return okAsync(res);
+    }
+  });
 }
