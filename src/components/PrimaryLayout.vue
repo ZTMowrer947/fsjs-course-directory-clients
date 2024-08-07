c65fd9a7fad9258eaf0427b8cc47dd22c84c5fd6
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-08-07 11:40:10 -0500

    Update PrimaryLayout to use new auth flow
    Clear credentials on auth failure

--- src/components/PrimaryLayout.vue
+++ src/components/PrimaryLayout.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex-1 flex flex-col">
-    <PrimaryHeader :user="data ?? undefined" :auth-pending="isPending" @signout="signOut" />
+    <PrimaryHeader
+      :user="authQuery.data.value ?? undefined"
+      :auth-pending="authQuery.isPending.value"
+      @signout="signOut"
+    />
     <div class="mx-10 mt-5 flex-1 flex flex-col" data-testid="main-content">
       <slot></slot>
     </div>
@@ -9,18 +13,25 @@
 
 <script setup lang="ts">
 import { useQuery, useQueryClient } from '@tanstack/vue-query';
-import { inject } from 'vue';
+import { inject, watch } from 'vue';
 
+import { AuthFailError } from '~/entities/errors';
 import { credentialManagerKey } from '~/injectKeys.ts';
 import { dummyCredentialManager } from '~/lib/credential.ts';
-import { authedUserQueryOpts } from '~/routes/(auth)/queries/byStored.ts';
+import { hydrateStoredUserOpts } from '~/routes/(auth)/queries/signin.ts';
 import { userKeys } from '~/routes/(auth)/queryKeys.ts';
 
 import PrimaryHeader from './PrimaryHeader.vue';
 
 const credentialManager = inject(credentialManagerKey, dummyCredentialManager);
-const { data, isPending } = useQuery(authedUserQueryOpts(credentialManager));
+const authQuery = useQuery(hydrateStoredUserOpts(credentialManager));
 const queryClient = useQueryClient();
+
+watch([authQuery.error, () => credentialManager.get()], ([error, credentials]) => {
+  if (error instanceof AuthFailError && credentials) {
+    credentialManager.clear();
+  }
+});
 
 function signOut() {
   // Remove credentials and invalidate user query
