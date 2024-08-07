aad31eab7ec4e07ec82b148ca4b5c6724d18e753
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-08-07 11:37:02 -0500

    Refactor user hydration with Result

--- src/routes/(auth)/queries/signin.ts
+++ src/routes/(auth)/queries/signin.ts
@@ -1,8 +1,12 @@
-import type { ResultAsync } from 'neverthrow';
+import type { QueryOptions } from '@tanstack/vue-query';
+import { errAsync, type ResultAsync } from 'neverthrow';
 
 import { AuthFailError, ResponseNotOkError, UnexpectedAppError } from '~/entities/errors.ts';
 import type { User } from '~/entities/user.ts';
-import { fetchAsResultAsync, jsonAsResultAsync } from '~/lib/result.ts';
+import type { ICredentialManager } from '~/lib/credential.ts';
+import { asyncUnwrapOrReject, fetchAsResultAsync, jsonAsResultAsync } from '~/lib/result.ts';
+
+import { userKeys } from '../queryKeys.ts';
 
 export type UserFromEncodedError = UnexpectedAppError | AuthFailError;
 
@@ -24,4 +28,33 @@ export function getUserFromEncodedCredentials(encoded: string): ResultAsync<User
       }
     })
     .map((data) => data as User);
+}
+
+export function getUserFromStoredCredentials(
+  credentialManager: ICredentialManager,
+): ResultAsync<User, UserFromEncodedError> {
+  const credentials = credentialManager.get();
+
+  // If there are no credentials stored, fail immediately
+  if (!credentials) {
+    return errAsync(new AuthFailError());
+  }
+
+  const encoded = credentialManager.encode(credentials);
+
+  return getUserFromEncodedCredentials(encoded);
+}
+
+export function hydrateStoredUserOpts(credentialManager: ICredentialManager) {
+  return {
+    queryKey: userKeys.user,
+    queryFn() {
+      const hydrateResult = getUserFromStoredCredentials(credentialManager);
+
+      return asyncUnwrapOrReject(hydrateResult);
+    },
+    retry(failCount, error) {
+      return !(error instanceof AuthFailError) && failCount < 3;
+    },
+  } satisfies QueryOptions<User, UserFromEncodedError>;
 }
