6b59bf4f73f3b097f5a3784892dd74d81852e637
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-08-08 07:57:04 -0500

    Tweak course list fetch to consider any error unexpected

--- src/routes/courses/queries/all.ts
+++ src/routes/courses/queries/all.ts
@@ -2,14 +2,19 @@ import type { QueryOptions } from '@tanstack/vue-query';
 import type { ResultAsync } from 'neverthrow';
 
 import type { CoursePreview } from '~/entities/course.ts';
-import { asyncUnwrapOrReject, fetchAsResultAsync, type FetchError,jsonAsResultAsync } from '~/lib/result.ts';
+import { UnexpectedAppError } from '~/entities/errors.ts';
+import { asyncUnwrapOrReject, fetchAsResultAsync, jsonAsResultAsync } from '~/lib/result.ts';
 
 import courseKeys from '../queryKeys.ts';
 
-export function getCourseList(): ResultAsync<CoursePreview[], FetchError> {
-  return fetchAsResultAsync('http://localhost:5000/api/courses')
-    .andThen(jsonAsResultAsync)
-    .map((data) => data as CoursePreview[]);
+export function getCourseList(): ResultAsync<CoursePreview[], UnexpectedAppError> {
+  return (
+    fetchAsResultAsync('http://localhost:5000/api/courses')
+      .andThen(jsonAsResultAsync)
+      // Any error here is unexpected
+      .mapErr((e) => (e instanceof UnexpectedAppError ? e : new UnexpectedAppError(e)))
+      .map((data) => data as CoursePreview[])
+  );
 }
 
 const allCoursesQueryOpts = {
